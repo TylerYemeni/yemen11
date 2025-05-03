3921b67a5cd7c86d235968fa728b879bae7fab6f
TylerYemeni <leomessi111909@gmail.com>
2025-05-03 18:39:05 +0300

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.example.yemen11"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.yemen11"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
